bfd28e7411e3eea82550460e51ebe9ff366bd6a6
Sky471012 <sharma.aakash1012@gmail.com>
2025-10-10 21:11:34 +0530

    Deploying on render

--- dm-featured/index.js
+++ dm-featured/index.js
@@ -9,6 +9,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import qrcode from "qrcode-terminal";
 
+const app = express();
+const port = 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
